26e6f8d890a45257145bda2183b0dd5bd36a568f
Deydeepya <ddpyawork@gmail.com>
2024-08-01 18:54:35 +0300

    final update on products

--- src/components/assets/all_product.js
+++ src/components/assets/all_product.js
@@ -28,120 +28,201 @@ import img26 from "./valo/poster.jpg";
 import img27 from "./valo/stickers.jpg";
 import img28 from "./valo/thirsty.jpg";
 
-let all_product= [
+let all_product = [
     {
         id: 1,
-        name: "Hand-sized Book", 
+        name: "Hand-sized Book",
         category: "Phasmophobia",
         image: img1,
-        price: 15
-    },
-    {
-        id: 2,
-        name: "Tarot Cards Crop Hoodie", 
-        category: "Phasmophobia",
-        image: img2,
-        price: 25
-    },
-    {
-        id: 3,
-        name: "Tarot Cards Hoodie", 
-        category: "Phasmophobia",
-        image: img3,
-        price: 27
-    },
-    {
-        id: 4,
-        name: "Catch Me On Phasmo T-shirt", 
-        category: "Phasmophobia",
-        image: img4,
         price: 10
     },
     {
+        id: 2,
+        name: "Tarot Cards Crop Hoodie",
+        category: "Phasmophobia",
+        image: img2,
+        price: 35
+    },
+    {
+        id: 3,
+        name: "Tarot Cards Hoodie",
+        category: "Phasmophobia",
+        image: img3,
+        price: 40
+    },
+    {
+        id: 4,
+        name: "Catch Me On Phasmo T-shirt",
+        category: "Phasmophobia",
+        image: img4,
+        price: 20
+    },
+    {
         id: 5,
-        name: "Navy-Blue Emote Cap", 
+        name: "Navy-Blue Emote Cap",
         category: "Phasmophobia",
         image: img5,
-        price: 25
+        price: 20
     },
     {
         id: 6,
-        name: "UV Hand-Print Crop-Hoodie", 
+        name: "UV Hand-Print Crop-Hoodie",
         category: "Phasmophobia",
         image: img6,
-        price: 14.5
+        price: 35
     },
     {
         id: 7,
-        name: "UV Hand-Print Hoodie", 
+        name: "UV Hand-Print Hoodie",
         category: "Phasmophobia",
         image: img7,
-        price: 21
+        price: 40
     },
     {
         id: 8,
-        name: "Haunted Mirror Keychain", 
+        name: "Haunted Mirror Keychain",
         category: "Phasmophobia",
         image: img8,
-        price: 7
+        price: 8
     },
     {
         id: 9,
-        name: "Ghost-Orb Hunting T-shirt", 
+        name: "Ghost-Orb Hunting T-shirt",
         category: "Phasmophobia",
         image: img9,
-        price: 9
+        price: 18
     },
     {
         id: 10,
-        name: "UV Hand-Print Mug", 
+        name: "UV Hand-Print Mug",
         category: "Phasmophobia",
         image: img10,
-        price: 17
+        price: 15
     },
     {
         id: 11,
-        name: "Music Box", 
+        name: "Music Box",
         category: "Phasmophobia",
         image: img11,
         price: 25
     },
     {
         id: 12,
-        name: "Glow-In-The-Dark Pendant", 
+        name: "Glow-In-The-Dark Pendant",
         category: "Phasmophobia",
         image: img12,
-        price: 12
+        price: 15
     },
     {
         id: 13,
-        name: "Life's A Revenant T-Shirt", 
+        name: "Life's A Revenant T-Shirt",
         category: "Phasmophobia",
         image: img13,
-        price: 9
+        price: 18
     },
     {
         id: 14,
-        name: "White Handprints T-Shirt", 
+        name: "White Handprints T-Shirt",
         category: "Phasmophobia",
         image: img14,
-        price: 8
+        price: 18
     },
     {
         id: 15,
-        name: "Tarot Card Themed Uno Cards", 
+        name: "Tarot Card Themed Uno Cards",
         category: "Phasmophobia",
         image: img15,
-        price: 13.5
+        price: 12
     },
     {
         id: 16,
-        name: "Voodoo Doll Plushy", 
-        category: "Phassmophobia",
+        name: "Voodoo Doll Plushy",
+        category: "Phasmophobia",
         image: img16,
-        price: 18.5
+        price: 25
     },
-    
-
-    
+    {
+        id: 17,
+        name: "Mind-blown Jett T-Shirt",
+        category: "Valorant",
+        image: img17,
+        price: 20
+    },
+    {
+        id: 18,
+        name: "Cat Beanie",
+        category: "Valorant",
+        image: img18,
+        price: 12
+    },
+    {
+        id: 19,
+        name: "Cypher Black Phone Cover",
+        category: "Valorant",
+        image: img19,
+        price: 15
+    },
+    {
+        id: 20,
+        name: "Cypher White Phone Cover",
+        category: "Valorant",
+        image: img20,
+        price: 15
+    },
+    {
+        id: 21,
+        name: "Deadlock T-shirt",
+        category: "Valorant",
+        image: img21,
+        price: 22
+    },
+    {
+        id: 22,
+        name: "Gekko T-Shirt",
+        category: "Valorant",
+        image: img22,
+        price: 20
+    },
+    {
+        id: 23,
+        name: "Cool Pixel Glasses",
+        category: "Valorant",
+        image: img23,
+        price: 10
+    },
+    {
+        id: 24,
+        name: "T-pose Omen T-Shirt",
+        category: "Valorant",
+        image: img24,
+        price: 22
+    },
+    {
+        id: 25,
+        name: "Penguin Dabbing T-shirt",
+        category: "Valorant",
+        image: img25,
+        price: 18
+    },
+    {
+        id: 26,
+        name: "Valorant Poster",
+        category: "Valorant",
+        image: img26,
+        price: 10
+    },
+    {
+        id: 27,
+        name: "Valorant Stickers",
+        category: "Valorant",
+        image: img27,
+        price: 8
+    },
+    {
+        id: 28,
+        name: "Yoru Bottle",
+        category: "Valorant",
+        image: img28,
+        price: 20
+    }
 ];
