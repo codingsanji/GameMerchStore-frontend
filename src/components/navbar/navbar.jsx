8466bff4d0489d20fbe50857d71bfe6bfbc70161
Deydeepya <ddpyawork@gmail.com>
2024-08-03 08:08:58 +0300

    navbar-final

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,17 +1,26 @@
-import React from 'react' 
-import './navbar.css'
-import logo from '../assets/logo.png'
-
+import React from 'react';
+import './navbar.css';
+import logo from '../assets/logo.png';
+import cart_icon from '../assets/cart.png';
 
 const Navbar = () => {
     return (
         <div className='navbar'>
-
+            <div className="nav-logo">
+                <img src={logo} alt="Logo" />
+            </div>
+            <ul className="nav-menu">
+                <li>Home</li>
+                <li>Phasmophobia</li>
+                <li>Valorant</li>
+                <li>Shop</li>
+            </ul>
+            <div className='nav-login-cart'>
+                <img src={cart_icon} alt="Cart" />
+                <button>Login</button>
+            </div>
         </div>
-
-    )
-        
-    
+    );
 }
 
-export default Navbar
+export default Navbar;
