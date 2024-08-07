b4c1e4523bb1daec928c29625de39376a4c6ea53
Deydeepya <ddpyawork@gmail.com>
2024-08-07 17:27:46 +0300

    fontadd+responsive

--- src/components/phasmo/phasguide.jsx
+++ src/components/phasmo/phasguide.jsx
@@ -26,23 +26,28 @@ const Phasguide = () => {
                 <img src={Middle} alt="Middle" className="w-full h-auto object-cover opacity-100" />
                 <div className="absolute top-0 left-0 w-full sm:w-4/5 md:w-3/4 lg:w-1/2 xl:w-2/5 2xl:w-1/3 p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12 2xl:p-14 flex flex-col items-start">
                     <h1 className="text-base sm:text-lg md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-bold text-white mb-4" 
-                        style={{ textShadow: '3px 1px 10px rgba(0, 0, 0, 2)' }}>
+                        style={{ textShadow: '3px 1px 10px rgba(0, 0, 0, 2)' , fontFamily: 'eco' }}>
                         OVER-VIEW
                     </h1>
                     <p className="text-xxs sm:text-xxs md:text-xs lg:text-base xl:text-base 2xl:text-lg text-white mb-4 sm:mb-6"
-                        style={{ textShadow: '3px 1px 10px rgba(0, 0, 0, 2)' }}>
+                        style={{ textShadow: '3px 1px 10px rgba(0, 0, 0, 2)' , fontFamily: 'spelite' }}>
                         Phasmophobia is a first-person horror-investigation-survival game for one to four players. The main goal is to identify the type of ghost haunting a location by gathering evidence. Players communicate via local (press "v" to toggle mute) and global (hold "b" to speak) voice chat. Evidence is logged in the ghost diary (press "j" to open).
                     </p>
                     <p className="text-xxs sm:text-xxs md:text-xs lg:text-base xl:text-base 2xl:text-lg text-white mb-4 sm:mb-6"
-                        style={{ textShadow: '3px 1px 10px rgba(0, 0, 0, 2)' }}>
+                        style={{ textShadow: '3px 1px 10px rgba(0, 0, 0, 2)' , fontFamily: 'spelite' }}>
                         Succeeding will earn you XP to level up and money which can be used to unlock new maps, levels, and upgrades for their equipment. Completing objectives and taking photos can enhance and double the rewards.
                     </p>
                     <p className="text-xxs sm:text-xxs md:text-xs lg:text-base xl:text-base 2xl:text-lg text-white"
-                        style={{ textShadow: '3px 1px 10px rgba(0, 0, 0, 2)' }}>
+                        style={{ textShadow: '3px 1px 10px rgba(0, 0, 0, 2)' , fontFamily: 'spelite' }}>
                         It was made by Kinetic Games in 2020.
                     </p>
                 </div>
             </div>
+
+
+
+
+
         </div>
     );
 }
