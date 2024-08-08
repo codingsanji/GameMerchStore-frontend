1df25f5fcc44bc6ad72fbe902f2deb04d89a2ef1
Deydeepya <ddpyawork@gmail.com>
2024-08-08 19:46:52 +0300

    phasmo-data

---
+++ src/components/assets/favphasmo.js
@@ -0,0 +1,36 @@
+import img1 from "./phasmo/memug.jpg";
+import img2 from "./phasmo/cardcrophood.jpg";
+import img3 from "./phasmo/musicbox.jpg";
+import img4 from "./phasmo/pendant.jpg";
+
+
+let favphasmo = [
+    {
+        id: 1,
+        name: "UV Hand-Print Mug",
+        category: "Phasmophobia",
+        image: img1,
+        price: 15
+    },
+    {
+        id: 2,
+        name: "Tarot Cards Crop Hoodie",
+        category: "Phasmophobia",
+        image: img2,
+        price: 35
+    },
+    {
+        id: 3,
+        name: "Music Box",
+        category: "Phasmophobia",
+        image: img3,
+        price: 25
+    },
+    {
+        id: 4,
+        name: "Glow-In-The-Dark Pendant",
+        category: "Phasmophobia",
+        image: img4,
+        price: 15
+    }
+];
---
+++ src/components/assets/valodata.js
@@ -0,0 +1,99 @@
+import img1 from "./valo/bruhwut.jpg";
+import img2 from "./valo/cat.jpg";
+import img3 from "./valo/cypherblack.jpg";
+import img4 from "./valo/cypherwhite.jpg";
+import img5 from "./valo/dloc.jpg";
+import img6 from "./valo/gekkotee.jpg";
+import img7 from "./valo/glass.jpg";
+import img8 from "./valo/omentee.jpg";
+import img9 from "./valo/pendab.jpg";
+import img10 from "./valo/poster.jpg";
+import img11 from "./valo/stickers.jpg";
+import img12 from "./valo/thirsty.jpg";
+
+let valoodata = [
+    {
+        id: 1,
+        name: "Mind-blown Jett T-Shirt",
+        category: "Valorant",
+        image: img1,
+        price: 20
+    },
+    {
+        id: 2,
+        name: "Cat Beanie",
+        category: "Valorant",
+        image: img2,
+        price: 12
+    },
+    {
+        id: 3,
+        name: "Cypher Black Phone Cover",
+        category: "Valorant",
+        image: img3,
+        price: 15
+    },
+    {
+        id: 4,
+        name: "Cypher White Phone Cover",
+        category: "Valorant",
+        image: img4,
+        price: 15
+    },
+    {
+        id: 5,
+        name: "Deadlock T-shirt",
+        category: "Valorant",
+        image: img5,
+        price: 22
+    },
+    {
+        id: 6,
+        name: "Gekko T-Shirt",
+        category: "Valorant",
+        image: img6,
+        price: 20
+    },
+    {
+        id: 7,
+        name: "Cool Pixel Glasses",
+        category: "Valorant",
+        image: img7,
+        price: 10
+    },
+    {
+        id: 8,
+        name: "T-pose Omen T-Shirt",
+        category: "Valorant",
+        image: img8,
+        price: 22
+    },
+    {
+        id: 9,
+        name: "Penguin Dabbing T-shirt",
+        category: "Valorant",
+        image: img9,
+        price: 18
+    },
+    {
+        id: 10,
+        name: "Valorant Poster",
+        category: "Valorant",
+        image: img10,
+        price: 10
+    },
+    {
+        id: 11,
+        name: "Valorant Stickers",
+        category: "Valorant",
+        image: img11,
+        price: 8
+    },
+    {
+        id: 12,
+        name: "Yoru Bottle",
+        category: "Valorant",
+        image: img12,
+        price: 20
+    }
+];
