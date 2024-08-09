4630d918b8842410280f016661d2c06ccf09ae96
Deydeepya <ddpyawork@gmail.com>
2024-08-09 20:22:48 +0300

    contact us rough design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Latest from "../components/latest/latest";
 
+
 const Home = () => {
   return (
     <div>
--- src/pages/Phasmo.jsx
+++ src/pages/Phasmo.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Phast from '../components/phasmo/phasguide'
 import PProducts from '../components/perproducts/pproducts'
+import ContactUs from '../components/Newsletter/Newsletter'
 
 const Phasmo = () => {
     return(
         <div>
             <Phast />
             <PProducts />
+            <ContactUs />
         </div>
     )
 }
