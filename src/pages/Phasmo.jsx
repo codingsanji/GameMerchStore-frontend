bac5c942ce75545e909db1aa986cc66a2f40b681
Deydeepya <ddpyawork@gmail.com>
2024-08-11 07:21:34 +0300

    shop-header + certain fixes

--- src/pages/Phasmo.jsx
+++ src/pages/Phasmo.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Phasguide from "../components/phasmo/phasguide";
 import PProducts from "../components/perproducts/pproducts";
+import ContactUs from "../components/Newsletter/Newsletter"
+import Footer from "../components/footer/footer"
 
 const Phasmo = () => {
   return (
     <div>
       <Phasguide />
       <PProducts />
+      <ContactUs />
+      <Footer />
     </div>
   );
 };
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Head from '../components/shopheader/header'
+import ContactUs from "../components/Newsletter/Newsletter"
+import Footer from "../components/footer/footer"
 
 const Shop = () => {
     return(
         <div>
             <Head />
+            <ContactUs />
+            <Footer />
         </div>
     )
 }
