fa65e77b39b4f57383205008fd47d02881454155
Deydeepya <ddpyawork@gmail.com>
2024-08-12 09:43:24 +0300

    minor changes

--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./items.css";
+import "./Items.css";
 
 const Item = (items) => {
   return (
--- src/components/latest/latest.jsx
+++ src/components/latest/latest.jsx
@@ -51,7 +51,6 @@ const Latest = () => {
   };
 
   return (
-<<<<<<< HEAD
     <div className="homepage text-white">
       {/* Hero Section */}
       <div className="container max-w-full pt-12 pb-16 px-8">
@@ -81,29 +80,6 @@ const Latest = () => {
             alt="Hero"
             className="w-auto m-auto max-w-full h-auto"
           />
-=======
-    <div className="container max-w-full pt-12 pb-16 px-8 bg-gray-800">
-      <div className="hero grid md:grid-cols-2 rounded-md">
-        <div className="flex flex-col text-white p-16 2xl:ml-[4rem] xl:ml-[2rem] md:text-left text-center">
-          <h2 className="text-[#ff00ff] font-bold xl:text-5xl lg:text-4xl md:text-3xl text-2xl">
-            FRESH AND FIERCE
-          </h2>
-          <p className="font-light xl:text-2xl lg:text-xl sm:text-2xl text-lg">
-            New👋
-          </p>
-          <p className="font-light xl:text-2xl lg:text-xl sm:text-2xl text-lg">
-            Collections
-          </p>
-          <p className="font-light xl:text-2xl lg:text-xl sm:text-2xl text-lg">
-            For everyone
-          </p>
-          <Link to="/shop">
-            <button className="bg-[#ff00ff] text-[rgb(43,39,91)] font-bold mt-4 p-1 lg:w-70 md:w-60 w-50 h-12 rounded-lg cursor-pointer md:text-lg text-sm transition-colors duration-300 ease-in-out">
-              LATEST COLLECTION{" "}
-              <FaLongArrowAltRight className="inline mb-1 text-xl" />
-            </button>
-          </Link>
->>>>>>> 139db2b85b7185c6c679d1fd883a7a678a6a3a85
         </div>
 
         {/* Featured Products Section */}
