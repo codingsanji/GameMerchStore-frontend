fb403bad9be2000f62a5d0345ea4d5b7a498c0be
Thanh Nguyen <nnnannn710@gmail.com>
2024-08-17 21:37:27 +0700

    initial commit

--- src/components/assets/weaponsdata.js
+++ src/components/assets/weaponsdata.js
@@ -1,28 +1,28 @@
-import ares from './valo/machine_guns/ares.png';
-import odin from './valo/machine_guns/odin.png';
+import ares from "./valo/machine_guns/ares.png";
+import odin from "./valo/machine_guns/odin.png";
 
-import melee from './valo/melee/melee.png';
+import melee from "./valo/melee/melee.png";
 
-import bulldog from './valo/rifles/bulldog.png';
-import guardian from './valo/rifles/guardian.png';
-import phantom from './valo/rifles/phantom.png';
-import vandal from './valo/rifles/vandal.png';
+import bulldog from "./valo/rifles/bulldog.png";
+import guardian from "./valo/rifles/guardian.png";
+import phantom from "./valo/rifles/phantom.png";
+import vandal from "./valo/rifles/vandal.png";
 
-import bucky from './valo/shotguns/bucky.png';
-import judge from './valo/shotguns/judge.png';
+import bucky from "./valo/shotguns/bucky.png";
+import judge from "./valo/shotguns/judge.png";
 
-import classic from './valo/sidearms/classic.png';
-import ghost from './valo/sidearms/ghost.png';
-import frenzy from './valo/sidearms/frenzy.png';
-import shorty from './valo/sidearms/shorty.png';
-import sheriff from './valo/sidearms/sheriff.png';
+import classic from "./valo/sidearms/classic.png";
+import ghost from "./valo/sidearms/ghost.png";
+import frenzy from "./valo/sidearms/frenzy.png";
+import shorty from "./valo/sidearms/shorty.png";
+import sheriff from "./valo/sidearms/sheriff.png";
 
-import spectre from './valo/smgs/spectre.png';
-import stinger from './valo/smgs/stinger.png';
+import spectre from "./valo/smgs/spectre.png";
+import stinger from "./valo/smgs/stinger.png";
 
-import marshal from './valo/sniper_rifles/marshal.png';
-import outlaw from './valo/sniper_rifles/outlaw.png';
-import operator from './valo/sniper_rifles/operator.png';
+import marshal from "./valo/sniper_rifles/marshal.png";
+import outlaw from "./valo/sniper_rifles/outlaw.png";
+import operator from "./valo/sniper_rifles/operator.png";
 
 let weapondata = [
   {
@@ -34,9 +34,7 @@ let weapondata = [
   },
   {
     category: "Melee",
-    items: [
-      { id: 3, name: "Default Knife", image: melee },
-    ],
+    items: [{ id: 3, name: "Default Knife", image: melee }],
   },
   {
     category: "Rifles",
@@ -88,21 +86,27 @@ export { weapondata };
 export const machineGuns = weapondata.find(
   (category) => category.category === "Machine Guns"
 ).items;
+
 export const melees = weapondata.find(
   (category) => category.category === "Melee"
 ).items;
+
 export const rifles = weapondata.find(
   (category) => category.category === "Rifles"
 ).items;
+
 export const shotguns = weapondata.find(
   (category) => category.category === "Shotguns"
 ).items;
+
 export const sidearms = weapondata.find(
   (category) => category.category === "Sidearms"
 ).items;
+
 export const smgs = weapondata.find(
   (category) => category.category === "SMGs"
 ).items;
+
 export const sniperRifles = weapondata.find(
   (category) => category.category === "Sniper Rifles"
 ).items;
--- src/components/val/weapons/gun.jsx
+++ src/components/val/weapons/gun.jsx
@@ -3,19 +3,14 @@ import "./weapons.css";
 
 const Gun = (gun) => {
   return (
-    <div className="bg-[#1c4659] w-full 2xl:m-4 p-4 pb-0 border-[2px] border-gray-600 shadow-md shadow-[#1a1a1a] rounded-sm guns">
+    <div className="guns uppercase text-sm text-[#ece8e1] bg-[#1c4659] w-full 2xl:m-4 p-4 pb-0 border-[2px] border-gray-600 shadow-md shadow-[#1a1a1a] rounded-sm">
       <img
         className="xl:h-[110px] lg:h-[100px] md:h-[90px] h-[70px] object-contain mx-auto"
         src={gun.image}
         alt={gun.name}
       />
       <span>
-        <p
-          className="uppercase text-[#ece8e1] text-sm"
-          style={{ fontFamily: "Arial, san-serif" }}
-        >
-          {gun.name}
-        </p>
+        <p style={{ fontFamily: "Arial, san-serif" }}>{gun.name}</p>
       </span>
     </div>
   );
