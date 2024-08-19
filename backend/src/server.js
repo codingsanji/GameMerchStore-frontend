c806f4a5a2924165447e70e1729b09b96603f7a8
Thanh Nguyen <nnnannn710@gmail.com>
2024-08-19 17:52:37 +0700

    backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,11 @@
-const express = require('express')
-const { DbContext, sequelize } = require('./dbcontext/sequelize')
+const express = require("express");
+const { DbContext, sequelize } = require("./dbcontext/sequelize");
 
-const app = express()
+const app = express();
 
 sequelize.authenticate().then(async () => {
-	app.listen(3001, () => {
-		console.log(`App is running on port 3001`)
-	})
-})
-app.use(express.json())
-
+  app.listen(3001, () => {
+    console.log(`App is running on port 3001`);
+  });
+});
+app.use(express.json());
