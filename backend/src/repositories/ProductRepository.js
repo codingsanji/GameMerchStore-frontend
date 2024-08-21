f9514bda99bd3b2e3fbf88db91965c7f70cce33a
Thanh Nguyen <nnnannn710@gmail.com>
2024-08-21 15:41:00 +0700

    backend

--- backend/src/repositories/ProductRepository.js
+++ backend/src/repositories/ProductRepository.js
@@ -1,6 +1,51 @@
+const { Op } = require('sequelize')
 const { DbContext } = require('../dbcontext/sequelize')
+const Category = require('../entities/Category')
 
 module.exports = {
-	findById: async (id) => await DbContext.Product.findByPk(id),
-	addProduct: (product) => DbContext.Product.create({ ...product }),
+	getAll: async () => await DbContext.Product.findAll({ include: 'Category' }),
+
+	getById: async (id) =>
+		await DbContext.Product.findByPk(id, { include: 'Category' }),
+
+	filterByCategory: async (categoryName) =>
+		await DbContext.Product.findAll({
+			include: [
+				{
+					model: DbContext.Category,
+					as: 'Category',
+					where: { name: categoryName },
+				},
+			],
+		}),
+
+	filterByNameContained: async (name) =>
+		await DbContext.Product.findAll({
+			where: {
+				name: { [Op.like]: `%${name}%` },
+			},
+		}),
+
+	filterByCategoryAndName: async (categoryName, name) =>
+		await DbContext.Product.findAll({
+			include: [
+				{
+					model: DbContext.Category,
+					as: 'Category',
+					where: { name: categoryName },
+				},
+			],
+			where: {
+				name: { [Op.like]: `%${name}%` },
+			},
+		}),
+
+	add: async (product) => await DbContext.Product.create({ ...product }),
+
+	update: async (product, newValue) => {
+		product.set({ ...newValue })
+		await product.save()
+	},
+
+	delete: async (product) => await product.destroy(),
 }
