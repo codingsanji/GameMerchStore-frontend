105ae28457f52f42801b599934575e44fa1ac18c
Thanh Nguyen <nnnannn710@gmail.com>
2024-08-22 19:15:54 +0700

    restore phasmo page

--- src/pages/Phasmo.jsx
+++ src/pages/Phasmo.jsx
@@ -1,30 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Phasguide from "../components/phasmo/phasguide";
 import PProducts from "../components/perproducts/pproducts";
 import ContactUs from "../components/Newsletter/Newsletter";
 import Footer from "../components/footer/footer";
-import axiosclient from "../utils/axiosclient";
-import { AssetImages } from "../utils/imageurl";
 
 const Phasmo = () => {
-  const [pproduct, setPproduct] = useState([]);
-  useEffect(() => {
-    axiosclient
-      .get("/product/filter?categoryName=Phasmophobia")
-      .then((response) => {
-        setPproduct(response.data);
-      })
-      .catch((error) => console.log(error));
-      pproduct.map(
-        (product) =>
-          (product.image = AssetImages.PhasmoImage + product.image)
-      );
-  }, []);
-
   return (
     <div>
       <Phasguide />
-      <PProducts filteredProducts={pproduct} />
+      <PProducts />
       <ContactUs />
       <Footer />
     </div>
