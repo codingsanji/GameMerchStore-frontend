255c3231cad5c18dc685537dd6f9e950a1cf5d4a
Deydeepya <ddpyawork@gmail.com>
2024-08-22 22:04:26 +0300

    last touches

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,53 +1,99 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axiosclient from "../utils/axiosclient";
+import Snackbar from "../components/snackbar/Snackbar";
 
 const Login = () => {
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-400 via-indigo-500 to-purple-500 p-4 overflow-hidden">
-            <div className="w-full max-w-md bg-white rounded-lg shadow-md p-6 -mt-12"> 
-                <h1 className="text-4xl font-bold text-center text-gray-700 -ml-0 md:-ml-7 pb-1 mb-4">Login</h1>
-                <div className="flex flex-col justify-center space-y-6">
-                    <div className="w-full space-y-4">
-                        <input 
-                            type="text" 
-                            placeholder="Email Address" 
-                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                        />
-                        <input 
-                            type="password" 
-                            placeholder="Password" 
-                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                        />
-                    </div>
-                    <button 
-                        className="w-full bg-indigo-600 text-white font-bold py-2 rounded-md hover:bg-indigo-700 transition duration-200"
-                    >
-                        Login
-                    </button>
-                    <Link to="/signup">
-                        <p className="text-center text-gray-600">
-                            Don't have an account? <span className="text-indigo-600 hover:underline cursor-pointer">Sign up here</span>.
-                        </p>
-                    </Link>
-                    <div className="flex items-center justify-between mt-4">
-                        <div className="flex items-center">
-                            <input 
-                                type="checkbox" 
-                                id="remember" 
-                                className="w-4 h-4 text-indigo-600 focus:ring-0 border-gray-300 rounded"
-                            />
-                            <label htmlFor="remember" className="ml-2 text-gray-600">
-                                Remember me
-                            </label>
-                        </div>
-                        <span className="text-indigo-600 hover:underline cursor-pointer">
-                            Forgot your password?
-                        </span>
-                    </div>
-                </div>
+  const [data, setData] = useState({ email: "", password: "" });
+  const [error, setError] = useState("");
+  const [showSnackbar, setShowSnackbar] = useState(false);
+  const navigate = useNavigate();
+
+  const handlingChange = (event) => {
+    const { name, value } = event.target;
+    setData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handlingSubmitForm = (event) => {
+    event.preventDefault();
+    setError("");
+    setShowSnackbar(false);
+    axiosclient
+      .post("/auth/login", data)
+      .then((response) => {
+        const token = response.data.token;
+        localStorage.setItem("authToken", token);
+        navigate('/');
+      })
+      .catch((err) => {
+        setError(`Error: ${err.response.data.error || "Login failed"}`);
+        setShowSnackbar(true);
+      });
+  };
+
+  const handleCloseSnackbar = () => {
+    setShowSnackbar(false);
+  };
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-400 via-indigo-500 to-purple-500 p-4 overflow-hidden">
+      <div className="w-full max-w-md bg-white rounded-lg shadow-md p-6 -mt-12">
+        <h1 className="text-4xl font-bold text-center text-gray-700 -ml-0 md:-ml-7 pb-1 mb-4">
+          Login
+        </h1>
+        <form
+          onSubmit={handlingSubmitForm}
+          className="flex flex-col justify-center space-y-6"
+        >
+          <div className="w-full space-y-4">
+            <input
+              name="email"
+              type="email"
+              value={data.email}
+              onChange={handlingChange}
+              placeholder="Email Address"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            />
+            <input
+              name="password"
+              type="password"
+              value={data.password}
+              onChange={handlingChange}
+              placeholder="Password"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            />
+          </div>
+          <button className="w-full bg-indigo-600 text-white font-bold py-2 rounded-md hover:bg-indigo-700 transition duration-200">
+            Login
+          </button>
+          <Link to="/signup">
+            <p className="text-center text-gray-600">
+              Don't have an account?{" "}
+              <span className="text-indigo-600 hover:underline cursor-pointer">
+                Sign up here
+              </span>
+              .
+            </p>
+          </Link>
+          <div className="flex items-center justify-between mt-4">
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                id="remember"
+                className="w-4 h-4 text-indigo-600 focus:ring-0 border-gray-300 rounded"
+              />
+              <label htmlFor="remember" className="ml-2 text-gray-600">
+                Remember me
+              </label>
             </div>
-        </div>
-    );
+            <span className="text-indigo-600 hover:underline cursor-pointer">
+              Forgot your password?
+            </span>
+          </div>
+        </form>
+        <Snackbar type="error" message={error} onClose={handleCloseSnackbar} />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/RegJoin.jsx
+++ src/pages/RegJoin.jsx
@@ -1,53 +1,117 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import axiosclient from "../utils/axiosclient";
+import Snackbar from "../components/snackbar/Snackbar";
 
 const RegJoin = () => {
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 p-4 overflow-hidden">
-            <div className="w-full max-w-md bg-white rounded-lg shadow-md p-6 -mt-12"> 
-                <h1 className="text-4xl font-bold text-center text-gray-700 -ml-0 md:-ml-7 pb-1 mb-4">Sign Up</h1>
-                <div className="flex flex-col justify-center space-y-6">
-                    <div className="w-full space-y-4">
-                        <input 
-                            type="text" 
-                            placeholder="Your Name" 
-                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
-                        />
-                        <input 
-                            type="text" 
-                            placeholder="Email Address" 
-                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
-                        />
-                        <input 
-                            type="password" 
-                            placeholder="Password" 
-                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
-                        />
-                    </div>
-                    <button 
-                        className="w-full bg-purple-600 text-white font-bold py-2 rounded-md hover:bg-purple-700 transition duration-200"
-                    >
-                        Continue
-                    </button>
-                    <Link to="/login">
-                        <p className="text-center text-gray-600">
-                            Already have an account? <span className="text-purple-600 hover:underline cursor-pointer">Login here</span>.
-                        </p>
-                    </Link>
-                    <div className="flex items-center mt-4">
-                        <input 
-                            type="checkbox" 
-                            id="agree" 
-                            className="w-4 h-4 text-purple-600 focus:ring-0 border-gray-300 rounded"
-                        />
-                        <label htmlFor="agree" className="ml-2 text-gray-600">
-                            By continuing, I agree to the <span className="text-purple-600 hover:underline cursor-pointer">terms of use</span> & <span className="text-purple-600 hover:underline cursor-pointer">privacy policy</span>.
-                        </label>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+  const [data, setData] = useState({ username: "", email: "", password: "" });
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [showErrorSnackbar, setShowErrorSnackbar] = useState(false);
+  const [showSuccessSnackbar, setShowSuccessSnackbar] = useState(false);
+
+  const handlingChange = (event) => {
+    const { name, value } = event.target;
+    setData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handlingSubmitForm = (event) => {
+    event.preventDefault();
+    setError("");
+    setShowErrorSnackbar(false);
+    axiosclient
+      .post("/auth/register", data)
+      .then((response) => {
+        setSuccess(response.data.success);
+        setShowSuccessSnackbar(true);
+      })
+      .catch((err) => {
+        setError(`Error: ${err.response.data.error || "Login failed"}`);
+        setShowErrorSnackbar(true);
+      });
+  };
+
+  const handleCloseSnackbar = () => {
+    setShowErrorSnackbar(false);
+    setShowSuccessSnackbar(false);
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 p-4 overflow-hidden">
+      <div className="w-full max-w-md bg-white rounded-lg shadow-md p-6 -mt-12">
+        <h1 className="text-4xl font-bold text-center text-gray-700 -ml-0 md:-ml-7 pb-1 mb-4">
+          Sign Up
+        </h1>
+        <form
+          className="flex flex-col justify-center space-y-6"
+          onSubmit={handlingSubmitForm}
+        >
+          <div className="w-full space-y-4">
+            <input
+              name="username"
+              type="text"
+              value={data.username}
+              onChange={handlingChange}
+              placeholder="Your Name"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
+            />
+            <input
+              name="email"
+              type="email"
+              value={data.email}
+              onChange={handlingChange}
+              placeholder="Email Address"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
+            />
+            <input
+              name="password"
+              type="password"
+              value={data.password}
+              onChange={handlingChange}
+              placeholder="Password"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-purple-600 text-white font-bold py-2 rounded-md hover:bg-purple-700 transition duration-200"
+          >
+            Continue
+          </button>
+          <Link to="/login">
+            <p className="text-center text-gray-600">
+              Already have an account?{" "}
+              <span className="text-purple-600 hover:underline cursor-pointer">
+                Login here
+              </span>
+              .
+            </p>
+          </Link>
+          <div className="flex items-center mt-4">
+            <input
+              type="checkbox"
+              id="agree"
+              className="w-4 h-4 text-purple-600 focus:ring-0 border-gray-300 rounded"
+            />
+            <label htmlFor="agree" className="ml-2 text-gray-600">
+              By continuing, I agree to the{" "}
+              <span className="text-purple-600 hover:underline cursor-pointer">
+                terms of use
+              </span>{" "}
+              &{" "}
+              <span className="text-purple-600 hover:underline cursor-pointer">
+                privacy policy
+              </span>
+              .
+            </label>
+          </div>
+        </form>
+        <Snackbar type="error" message={error} onClose={handleCloseSnackbar} />
+        <Snackbar type="success" message={success} onClose={handleCloseSnackbar} />
+      </div>
+    </div>
+  );
 };
 
 export default RegJoin;
